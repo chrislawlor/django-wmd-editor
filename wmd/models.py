30ac48076385164c287858b958ee2f697e3fd7ff
Chris Lawlor <lawlor.chris@gmail.com>
2011-07-27 12:23:46 -0400

    Adds introspection rules for South

--- wmd/models.py
+++ wmd/models.py
@@ -2,6 +2,12 @@
 from django.db import models
 from django.contrib.admin import widgets as admin_widgets
 from wmd import widgets as wmd_widgets
+try:
+    from south.modelsinspector import add_introspection_rules
+    add_introspection_rules([], ["^wmd.models.MarkDownField"])
+except ImportError:
+    pass
+
 
 class MarkDownField(models.TextField):
     def formfield(self, **kwargs):
